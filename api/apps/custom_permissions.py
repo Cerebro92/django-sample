af788d93ad29c5420ce6814f8c39ddb9e0111f1a
Akhil Chakicherla <cvakhil@skilline.ai>
2023-03-12 01:25:43 +0530

    Enhancement: Added custom permission classes

---
+++ api/apps/custom_permissions.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsVerifiedUser(BasePermission):
+    """
+    Allows access only to authenticated users.
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            request.user and request.user.is_authenticated and request.user.is_verified
+        )
