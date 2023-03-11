cb6b6567383f8be653545d3f336859ea1859560a
Akhil Chakicherla <cvakhil@skilline.ai>
2023-03-12 01:26:29 +0530

    BugFix: Setting is_active False for invites

--- api/apps/accounts/serializers.py
+++ api/apps/accounts/serializers.py
@@ -2,55 +2,67 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.response import Response
 from .models import Invites
+from uuid import uuid4
+from rest_framework.serializers import ModelSerializer
+
+
 class UserSerializer(serializers.ModelSerializer):
     """
     Serializer for custom user model.
     """
+
     full_name = serializers.SerializerMethodField()
 
     class Meta:
         User = get_user_model()
         model = User
         fields = [
-            'id',
-            'email',
-            'password',
-            'first_name',
-            'last_name',
-            'full_name',
-            'is_verified',
+            "id",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "full_name",
+            "is_verified",
         ]
         extra_kwargs = {
-            'password': {'write_only': True},
+            "password": {"write_only": True},
         }
 
     def create(self, validated_data):
         User = get_user_model()
 
         # Add username to the objects before creation
-        validated_data['username'] = validated_data['email']
+        validated_data["username"] = validated_data["email"]
         return User.objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
         # Ensure that the email and password never get updated this way
-        validated_data.pop('email', None)
-        validated_data.pop('password', None)
+        validated_data.pop("email", None)
+        validated_data.pop("password", None)
         return super().update(instance, validated_data)
 
     def get_full_name(self, obj):
         return obj.get_full_name()
 
-from rest_framework.serializers import ModelSerializer
 
 class InvitesSeirializer(ModelSerializer):
-
     def update(self, instance, validated_data):
-        get_user_model().objects.create(**{
-            "email":instance.email,
-            "first_name":instance.first_name,
-            "last_name":instance.last_name
-        })
+        user_data = instance.__dict__
+        user_data["password"] = uuid4().__str__()
+        serializer = UserSerializer(data=user_data)
+        serializer.is_valid(raise_exception=True)  # Validation of User Data
+        user_obj = serializer.create(
+            {
+                "email": user_data["email"],
+                "password": user_data["password"],
+                "first_name": user_data["first_name"],
+                "last_name": user_data["last_name"],
+            }
+        )  # User Creation
+        validated_data["is_active"] = False  # Updating is_active to False of the Invite
         return super().update(instance, validated_data)
+
     class Meta:
         model = Invites
-        fields = '__all__'
+        fields = "__all__"
